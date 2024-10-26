0b298f1aa49fb556b56099f10ceef46db9e01270
Rick Mastry <jahrik@hotmail.com>
2024-10-26 13:34:26 -0400

    added path server js

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ import router from './routes/index.js'
 import routerOne from './routes/daily.js'
 import routerTwo from './routes/geolocate.js'
 
+const path = require('path')
 const PORT = process.env.PORT || 3000
 
 const app = express()
 //app.use(express.urlencoded({extended: true}));
 
-app.use(express.static('public'))
 
 app.use('/api', router)
 app.use('/onecall', routerOne)
 app.use('/locate', routerTwo)
+
+app.use(express.static('public'))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "index.html"))
+})
+
+
 
 
 
